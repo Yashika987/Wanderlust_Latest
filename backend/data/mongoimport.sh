827be99ebc4cd24c36efdf52723fd99c0f79ea8a
Ubuntu <ubuntu@ip-172-31-89-41.ec2.internal>
2025-01-11 13:49:07 +0000

    Fixed Docker compose issues

--- backend/data/mongoimport.sh
+++ backend/data/mongoimport.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
+
+echo "Waiting for MongoDB to start..."
+until mongo --host mongo-service --eval "print(\"MongoDB is up\")"; do
+    sleep 2
+done
+
 echo "Importing data into MongoDB..."
-mongoimport --host mongo-service --db wanderlust --collection posts --file /data/sample_posts.json --jsonArray
+mongoimport --host mongo-service --db wanderlust --collection posts --file /docker-entrypoint-initdb.d/sample_posts.json --jsonArray
+echo "Data imported successfully!"
 
